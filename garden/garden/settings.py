8ef991d4ebb48f53c54caccad09eef6b9bcb297f
Meiad Noushadi <meiadnoushadi@gmail.com>
2023-01-08 00:28:38 +0330

    setup main and blog applications

--- garden/garden/settings.py
+++ garden/garden/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'apps.main.apps.MainConfig',
+    'apps.blog.apps.BlogConfig',
 ]
 
 MIDDLEWARE = [
