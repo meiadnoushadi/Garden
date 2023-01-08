b69ab2542c61ca3c1f2a616e0867b006623486aa
Meiad Noushadi <meiadnoushadi@gmail.com>
2023-01-08 16:09:27 +0330

    add function in main.views and set in context_proc
    create function in views for access to media in
    all views appliaction main and set in settings
    project template in -> context_processors

--- garden/garden/settings.py
+++ garden/garden/settings.py
@@ -64,6 +64,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'apps.main.views.media_admin',
             ],
         },
     },
@@ -115,9 +116,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'fa-ir'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Tehran'
 
 USE_I18N = True
 
